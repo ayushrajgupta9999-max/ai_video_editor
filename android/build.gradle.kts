8dbdae3a2426fbe03a681cba7da6f2e683a6e5b5
ayushrajgupta9999-max <ayushrajgupta9999@gmail.com>
2026-08-29 09:22:57 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,18 +1,28 @@
-allprojects {
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        val propertiesFile = java.io.File(settingsDir, "local.properties")
+        if (propertiesFile.exists()) {
+            propertiesFile.reader(Charsets.UTF_8).use { properties.load(it) }
+        }
+        val sdkPath = properties.getProperty("flutter.sdk")
+        require(sdkPath != null) { "flutter.sdk not set in local.properties" }
+        sdkPath
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
-rootProject.buildDir = "../build"
-subprojects {
-    project.buildDir = "${rootProject.buildDir}/${project.name}"
-}
-subprojects {
-    project.evaluationDependsOn(":app")
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") apply false
+    id("org.jetbrains.kotlin.android") apply false
 }
 
-tasks.register("clean", Delete) {
-    delete rootProject.buildDir
-}
+include(":app")
